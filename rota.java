fd9384a07c727cd9f1322a1f39f7052674cc3792
EliWTi <eliezerlopes.ti@gmail.com>
2025-03-24 10:56:03 -0300

    Create rota.java

---
+++ rota.java
@@ -0,0 +1,49 @@
+public class rota {
+    private int id_rota;
+    private String nome;
+    private String descricao;
+    private double distancia;
+    private double duracao;
+
+    public int getId_rota() {
+        return id_rota;
+    }
+
+    public void setId_rota(int id_rota) {
+        this.id_rota = id_rota;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(double distancia) {
+        this.distancia = distancia;
+    }
+
+    public double getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(double duracao) {
+        this.duracao = duracao;
+    }
+    
+    
+}
