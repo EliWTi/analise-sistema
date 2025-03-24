1c113a0d135e884e20924a940776da3bc928961b
EliWTi <eliezerlopes.ti@gmail.com>
2025-03-24 10:57:02 -0300

    Create horario.java

---
+++ horario.java
@@ -0,0 +1,31 @@
+public class horario {
+    private int id_horario;
+    private int hora_partida;
+    private int hora_chegada;
+
+    public int getId_horario() {
+        return id_horario;
+    }
+
+    public void setId_horario(int id_horario) {
+        this.id_horario = id_horario;
+    }
+
+    public int getHora_partida() {
+        return hora_partida;
+    }
+
+    public void setHora_partida(int hora_partida) {
+        this.hora_partida = hora_partida;
+    }
+
+    public int getHora_chegada() {
+        return hora_chegada;
+    }
+
+    public void setHora_chegada(int hora_chegada) {
+        this.hora_chegada = hora_chegada;
+    }
+    
+    
+}
