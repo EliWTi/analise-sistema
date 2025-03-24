492c769f077265aa1deb4db0de777d48d658d161
EliWTi <eliezerlopes.ti@gmail.com>
2025-03-24 10:57:25 -0300

    Create cartao.java

---
+++ cartao.java
@@ -0,0 +1,69 @@
+import java.util.Date;
+
+public class cartao {
+    private int id_cartao;
+    private String numero;
+    private Date data;
+    private String cvv;
+    private String cpf;
+    private String nome_titular;
+    private usuario usuario;
+
+    public int getId_cartao() {
+        return id_cartao;
+    }
+
+    public void setId_cartao(int id_cartao) {
+        this.id_cartao = id_cartao;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome_titular() {
+        return nome_titular;
+    }
+
+    public void setNome_titular(String nome_titular) {
+        this.nome_titular = nome_titular;
+    }
+
+    public usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+    
+}
